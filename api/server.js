2e4c3a9c62952977a5d62bdb8ff94e13e75923d3
Anton Yarmak <perfect583@gmail.com>
2023-09-01 10:28:48 +0300

    API class implemented, CORS problem fixed

--- api/server.js
+++ api/server.js
@@ -1,9 +1,8 @@
 const http = require("http");
 const bodyParser = require("body-parser");
+const url = require("url");
 
 let todos = [];
-
-const defaultHeader = { "Content-Type": "application/json" };
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -11,15 +10,31 @@ const port = 3000;
 const jsonParser = bodyParser.json();
 
 const server = http.createServer((req, res) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+    "Access-Control-Max-Age": 2592000,
+    "Access-Control-Allow-Headers": "Content-Type",
+  };
+  if (req.method === "OPTIONS") {
+    res.writeHead(204, headers);
+    res.end();
+    return;
+  }
+
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+
   if (req.url === "/todos") {
     switch (req.method) {
       case "GET":
-        res.writeHead(200, defaultHeader);
+        res.writeHead(200, headers);
         res.end(JSON.stringify(todos));
         break;
       case "POST":
         jsonParser(req, res, () => {
           const { text } = req.body;
+          console.log("post testing");
           if (
             text !== "" &&
             text.trim().length >= 3 &&
@@ -30,40 +45,58 @@ const server = http.createServer((req, res) => {
               text: text,
               completed: false,
             });
-            res.writeHead(200, defaultHeader);
+
+            if (text === "error") {
+              res.writeHead(400, headers);
+              res.end(JSON.stringify({ message: "Some text for error" }));
+              return;
+            }
+
+            res.writeHead(201, headers);
             res.end(JSON.stringify(todos));
           } else {
-            res.writeHead(400, defaultHeader);
+            res.writeHead(400, headers);
             res.end(JSON.stringify({ message: "Invalid length" }));
           }
-        });
-        break;
-      case "DELETE":
-        jsonParser(req, res, () => {
-          const id = req.body.id;
-          const index = todos.findIndex((todo) => todo.id === id);
-          todos.splice(index, 1);
-          res.writeHead(200, defaultHeader);
-          res.end(JSON.stringify(todos));
         });
         break;
       case "PUT":
         jsonParser(req, res, () => {
           const { id, text } = req.body;
-          if (
-            text !== "" &&
-            text.trim().length >= 3 &&
-            text.trim().length < 20
-          ) {
-            const index = todos.findIndex((todo) => todo.id === id);
-            todos[index].text = text;
-            res.writeHead(200, defaultHeader);
-            res.end(JSON.stringify(todos));
-          } else {
-            res.writeHead(400, defaultHeader);
-            res.end(JSON.stringify({ message: "Invalid length" }));
+          const index = todos.findIndex((todo) => todo.id === id);
+          if (index === -1) {
+            res.writeHead(404, headers);
+            res.end(JSON.stringify({ message: "Todo not found" }));
+            return;
           }
+
+          if (text === "changingErr") {
+            res.writeHead(403, headers);
+            res.end(JSON.stringify({ message: "changing errrrrorr" }));
+            return;
+          }
+
+          if (id && text === undefined)
+            todos[index].completed = !todos[index].completed;
+          if (text) todos[index].text = text;
+          res.writeHead(200, headers);
+          res.end(JSON.stringify(todos));
         });
+        break;
+      default:
+        res.writeHead(405, headers);
+        res.end("Method not Allowed");
+        break;
+    }
+  }
+
+  if (pathname.startsWith("/todos/")) {
+    switch (req.method) {
+      case "DELETE":
+        const idToDelete = parseInt(pathname.split("/")[2]);
+        todos = todos.filter((todo) => todo.id !== idToDelete);
+        res.writeHead(200, headers);
+        res.end(JSON.stringify(todos));
         break;
       default:
         break;
@@ -72,17 +105,24 @@ const server = http.createServer((req, res) => {
 
   if (req.url === "/todos/bulk") {
     switch (req.method) {
-      case "PUT":
-        todos.forEach((todo) => todo.completed = true);
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify(todos));
-        break;
       case "DELETE":
+        console.log("delete testing");
         todos = todos.filter((todo) => todo.completed === false);
-        res.writeHead(200, { "Content-Type": "application/json" });
+        res.writeHead(200, headers);
+        // res.end(JSON.stringify(todos));
+        break;
+      case "PUT":
+        const allTodosCompleted = todos.every((todo) => todo.completed);
+        todos = todos.map((todo) => ({
+          ...todo,
+          completed: !allTodosCompleted,
+        }));
+        res.writeHead(200, headers);
         res.end(JSON.stringify(todos));
         break;
       default:
+        res.writeHead(405, headers);
+        res.end("Method Not Allowed");
         break;
     }
   }
